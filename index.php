e0360d5ba7059de9b47bfe2a91798a04c2960b28
uukrul <uukrul@asm003>
2013-03-18 23:09:05 +0200

    `

--- index.php
+++ index.php
@@ -1,12 +1,46 @@
 <?php
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  * 
  */
-echo "ExaMPLE";
-echo "-------";
 
+$argv1=array('./data/file.csv');
+$m=new Main($argv1);
+$m->parse();
+
+class Main extends csv {
+
+  function __construct($argv) {
+    self::parent($argv);
+    
+  }
+
+}
+
+class csv {
+
+  private $filename;
+  private $filedescr;
+
+  function __construct($filename) {
+    $this->filename = $filename;
+    $this->filedescr = fopen('Num.csv', 'r');
+  }
+
+  function destructor() {
+    if ($this->filedescr)
+      fclose($this->filedescr);
+  }
+
+  function parse() {
+    while (($data = fgetcsv($file, 1000, ';')) !== FALSE) {
+
+      $sum = $sum + $data[1];
+      echo $sum;
+    }
+  }
+
+}
 
 ?>
